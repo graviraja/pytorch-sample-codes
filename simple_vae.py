c6998645b35f6a48c8c88cae1743ae205bf35a5b
Raviraja <raviraja.ghanta@gmail.com>
2019-02-07 08:07:45 +0530

    modified implementation

--- simple_vae.py
+++ simple_vae.py
@@ -4,20 +4,39 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-import torch.autograd as autograd
 import torch.optim as optim
-import numpy as np
 
-from torch.autograd import Variable
-from tensorflow.examples.tutorials.mnist import input_data
+from torch.utils.data import DataLoader
+from torchvision import datasets, transforms
 
-mnist = input_data.read_data_sets('./data', one_hot=True)
-batch_size = 64
-z_dim = 100
-x_dim = mnist.train.images.shape[1]
-y_dim = mnist.train.labels.shape[1]
-h_dim = 128
-c = 0
+import matplotlib.pyplot as plt
+
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+BATCH_SIZE = 64
+N_EPOCHS = 10
+INPUT_DIM = 28 * 28
+HIDDEN_DIM = 256
+LATENT_DIM = 20
+
+transforms = transforms.Compose([transforms.ToTensor()])
+train_dataset = datasets.MNIST(
+    './data',
+    train=True,
+    download=True,
+    transform=transforms)
+
+test_dataset = datasets.MNIST(
+    './data',
+    train=False,
+    download=True,
+    transform=transforms
+)
+
+train_iterator = DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True)
+test_iterator = DataLoader(test_dataset, batch_size=BATCH_SIZE)
+
 lr = 1e-3
 
 
@@ -48,49 +67,102 @@ class Decoder(nn.Module):
 
     def forward(self, x):
         hidden = F.relu(self.linear(x))
-        predicted = F.sigmoid(self.out(hidden))
+        predicted = torch.sigmoid(self.out(hidden))
         return predicted
 
 
 class VAE(nn.Module):
-    def __init__(self, enc, dec, z_dim):
+    def __init__(self, enc, dec):
         super().__init__()
 
-        self.z_dim = z_dim
         self.enc = enc
         self.dec = dec
 
-    def sample(self, mu, log_var, batch_size):
-        eps = Variable(torch.randn(batch_size, self.z_dim))
-        return mu + torch.exp(log_var / 2) * eps
-
     def forward(self, x):
-        batch_size = x.shape[0]
+        # encode
         z_mu, z_var = self.enc(x)
-        x_sample = self.sample(z_mu, z_var, batch_size)
+
+        # reparameterize
+        std = torch.exp(z_var / 2)
+        eps = torch.randn_like(std)
+        x_sample = eps.mul(std).add_(z_mu)
+
+        # decode
         predicted = self.dec(x_sample)
         return predicted, z_mu, z_var
 
-encoder = Encoder(x_dim, h_dim, z_dim)
-decoder = Decoder(z_dim, h_dim, x_dim)
-model = VAE(encoder, decoder, z_dim)
+encoder = Encoder(INPUT_DIM, HIDDEN_DIM, LATENT_DIM)
+decoder = Decoder(LATENT_DIM, HIDDEN_DIM, INPUT_DIM)
+model = VAE(encoder, decoder).to(device)
 
 optimizer = optim.Adam(model.parameters(), lr=lr)
 
-for epoch in range(100000):
-    X, _ = mnist.train.next_batch(batch_size)
-    X = Variable(torch.from_numpy(X))
 
-    optimizer.zero_grad()
-    X_sample, z_mu, z_var = model(X)
+def train():
+    model.train()
+    train_loss = 0
 
-    recon_loss = F.binary_cross_entropy(X_sample, X)
-    kl_loss = 0.5 * torch.sum(torch.exp(z_var) + z_mu**2 - 1.0 - z_var)
+    for i, (x, _) in enumerate(train_iterator):
+        x = x.view(-1, 28 * 28)
+        x = x.to(device)
+        optimizer.zero_grad()
+        x_sample, z_mu, z_var = model(x)
 
-    loss = recon_loss + kl_loss
+        recon_loss = F.binary_cross_entropy(x_sample, x, size_average=False)
+        kl_loss = 0.5 * torch.sum(torch.exp(z_var) + z_mu**2 - 1.0 - z_var)
+        loss = recon_loss + kl_loss
+        loss.backward()
+        train_loss += loss.item()
+        optimizer.step()
 
-    loss.backward()
-    optimizer.step()
+    return train_loss
 
-    if (epoch + 1) % 1000 == 0:
-        print(f"epoch : {epoch}, loss: {loss.item()}")
+
+def test():
+    model.eval()
+    test_loss = 0
+
+    with torch.no_grad():
+        for i, (x, _) in enumerate(test_iterator):
+            x = x.view(-1, 28 * 28)
+            x = x.to(device)
+            x_sample, z_mu, z_var = model(x)
+
+            recon_loss = F.binary_cross_entropy(x_sample, x, size_average=False)
+            kl_loss = 0.5 * torch.sum(torch.exp(z_var) + z_mu**2 - 1.0 - z_var)
+
+            loss = recon_loss + kl_loss
+            test_loss += loss.item()
+
+    return test_loss
+
+best_test_loss = float('inf')
+
+for e in range(N_EPOCHS):
+
+    train_loss = train()
+    test_loss = test()
+
+    train_loss /= len(train_dataset)
+    test_loss /= len(test_dataset)
+
+    print(f'Epoch {e}, Train Loss: {train_loss:.2f}, Test Loss: {test_loss:.2f}')
+
+    if best_test_loss > test_loss:
+        best_test_loss = test_loss
+        patience_counter = 1
+    else:
+        patience_counter += 1
+
+    if patience_counter > 3:
+        break
+
+# sample and generate a image
+z = torch.randn(1, LATENT_DIM).to(device)
+reconstructed_img = model.dec(z)
+img = reconstructed_img.view(28, 28).data
+
+print(z.shape)
+print(img.shape)
+
+plt.imshow(img, cmap='gray')
