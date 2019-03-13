b8a4352d94473e2d39b539417b5e4e2a4c672785
Raviraja <raviraja.ghanta@gmail.com>
2019-03-13 22:20:30 +0530

    multi layer uni layer code added

--- hidden_rnn.py
+++ hidden_rnn.py
@@ -63,24 +63,46 @@ class Single_Layer_Uni_Directional_RNN(nn.Module):
 
         return output, hidden
 
-n_layers = 1
+# n_layers = 1
+# bidirectional = False
+# model = Single_Layer_Uni_Directional_RNN(input_dim, embedding_dim, hidden_dim, n_layers, bidirectional)
+# output, hidden = model(sequence_tensor)
+
+# print(f"Input shape is : {sequence_tensor.shape}")
+# print(f"Output shape is : {output.shape}")
+# print(f"Hidden shape is : {hidden.shape}")
+
+# assert (output[-1, :, :] == hidden[0]).all(), "Final output must be same as Hidden state in case of Single layer uni-directional RNN"
+
+
+class Multi_Layer_Uni_Directional_RNN(nn.Module):
+    def __init__(self, input_dim, embedding_dim, hidden_dim, n_layers, bidirectional):
+        super().__init__()
+        self.embedding = nn.Embedding(input_dim, embedding_dim)
+        self.rnn = nn.RNN(embedding_dim, hidden_dim, num_layers=n_layers, bidirectional=bidirectional)
+
+    def forward(self, input):
+        # input shape => [max_len, batch_size]
+
+        embed = self.embedding(input)
+        # embed shape => [max_len, batch_size, embedding_dim]
+
+        output, hidden = self.rnn(embed)
+        # output shape => [max_len, batch_size, hidden_size]
+        # hidden shape => [1, batch_size, hidden_size]
+
+        return output, hidden
+
+n_layers = 2
 bidirectional = False
-model = Single_Layer_Uni_Directional_RNN(input_dim, embedding_dim, hidden_dim, n_layers, bidirectional)
+model = Multi_Layer_Uni_Directional_RNN(input_dim, embedding_dim, hidden_dim, n_layers, bidirectional)
 output, hidden = model(sequence_tensor)
 
 print(f"Input shape is : {sequence_tensor.shape}")
 print(f"Output shape is : {output.shape}")
 print(f"Hidden shape is : {hidden.shape}")
 
-assert (output[-1, :, :] == hidden[0]).all(), "Final output must be same as Hidden state in case of Single layer uni-directional RNN"
-
-
-class Multi_Layer_Uni_Directional_RNN(nn.Module):
-    def __init__(self):
-        super().__init__()
-
-    def forward(self):
-        pass
+assert (output[-1, :, :] == hidden[-1]).all(), "Final output must be same as Final Hidden state in case of Multi layer uni-directional RNN"
 
 
 class Single_Layer_Bi_Directional_RNN(nn.Module):
